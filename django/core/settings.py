d69ff846beaba7121b68ed6198a62e3046127784
Mike Allaway <m.j.allaway@bham.ac.uk>
2023-12-10 09:21:59 +0000

    updated version number

--- django/core/settings.py
+++ django/core/settings.py
@@ -3,7 +3,7 @@ import sys
 
 
 # Version of this project (see: https://semver.org/)
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
