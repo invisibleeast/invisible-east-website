a81b19ab261c4da88b0568045f4afd585d111d70
Mike Allaway <m.j.allaway@bham.ac.uk>
2024-03-13 09:12:38 +0000

    used a more suitable font to allow for special chars

--- django/core/settings.py
+++ django/core/settings.py
@@ -3,7 +3,7 @@ import sys
 
 
 # Version of this project (see: https://semver.org/)
-VERSION = '1.0.7'
+VERSION = '1.0.8'
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
