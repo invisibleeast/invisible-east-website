acc2122b814feb3e388d33bae90eeb3356527422
Mike Allaway <m.j.allaway@bham.ac.uk>
2024-05-21 11:58:56 +0100

    v1.0.11 settings

--- django/core/settings.py
+++ django/core/settings.py
@@ -3,7 +3,7 @@ import sys
 
 
 # Version of this project (see: https://semver.org/)
-VERSION = '1.0.10'
+VERSION = '1.0.11'
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
